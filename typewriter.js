755ea794975e5de56d8c414531066dee9a967954
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-11 16:10:21 -0700

    Sumbitting first version of typewritter

---
+++ typewriter.js
@@ -0,0 +1,19 @@
+const sentence = process.argv.slice(2).join(" ");
+const newLineDelay = sentence.length * 50;
+
+let time = 0;
+for (const char of sentence) {
+  delayPrint(char, time);
+  time += 50;
+}
+
+function delayPrint(char, time) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time)
+};
+
+setTimeout(() => console.log("\n"), newLineDelay);
+
+
+
